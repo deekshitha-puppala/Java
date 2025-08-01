eb965856f679f88bf7a28b8f2a4853520d742c5a
Puppala Deeksitha <puppaladeekshitha2@gmail.com>
2025-08-01 23:32:34 +0530

    first commit

--- _01_FlipMessage.java
+++ _01_FlipMessage.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class FlipMessage {
+public class _01_FlipMessage {
      public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter String to be flipped: ");
